a9ec907c5dfda245d94e598a7bb26ddcb42a439a
albayerga <alba.yerga01@estudiant.upf.edu>
2023-11-14 19:55:10 +0100

    minimax y alphabeta

--- lab2/multiagent/multiAgents.py
+++ lab2/multiagent/multiAgents.py
@@ -128,6 +128,7 @@ class MultiAgentSearchAgent(Agent):
         self.evaluationFunction = util.lookup(evalFn, globals())
         self.depth = int(depth)
 
+
 class MinimaxAgent(MultiAgentSearchAgent):
     """
     Your minimax agent (question 2)
@@ -157,10 +158,29 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         "*** YOUR CODE HERE ***"
+        return self.max_pacman(gameState, 0, 0)[0]  #We are going to work with a 2 pos array where index 0 will be state actions and index 1 the actions value
 
-        return self.minimax(gameState, 0, 0)[0]
+    def minimax(self, gameState, agentIndex, depth): #MiniMax recursive primary function
+        if (depth == (self.depth * gameState.getNumAgents())) or gameState.isWin() or gameState.isLose(): #Return when we are on a final state
+            return self.evaluationFunction(gameState)
+        if agentIndex == 0: #When we are pacman (max)
+            return self.max_pacman(gameState, agentIndex, depth)[1]
+        else: #When we are ghosts (min)
+            return self.min_ghost(gameState, agentIndex, depth)[1]
 
-        #continuará
+    def max_pacman(self, gameState, agentIndex, depth):  #Max function
+        highestAction = ("",-float('inf'))
+        for action in gameState.getLegalActions(agentIndex):
+            succAction = (action,self.minimax(gameState.generateSuccessor(agentIndex,action), (depth + 1) % gameState.getNumAgents(), depth+1))
+            highestAction = max(highestAction,succAction,key=lambda x:x[1]) #We want to compare action value for each of them and take the maximum one
+        return highestAction
+
+    def min_ghost(self, gameState, agentIndex, depth):
+        lowestAction = ("",float('inf'))
+        for action in gameState.getLegalActions(agentIndex):
+            succAction = (action,self.minimax(gameState.generateSuccessor(agentIndex,action), (depth + 1) % gameState.getNumAgents(), depth+1))
+            lowestAction = min(lowestAction,succAction,key=lambda x:x[1])  #We want to compare action value for each of them and take the minimum one
+        return lowestAction
 
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
@@ -173,7 +193,44 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.max_pacman(gameState, 0, 0, -float("inf"), float("inf"))[0]  #Again, we are going to use a two position variable, our return will be the actions list
+
+    def alphabeta(self, gameState, agentIndex, depth, alpha, beta):  #In this case we are going to copy the same recursive code structure of minimax algorythm. Alpha and beta now are part of our function arguments
+        if (depth is self.depth * gameState.getNumAgents()) or gameState.isLose() or gameState.isWin():  #Access when we are on a recursive final state
+            return self.evaluationFunction(gameState)
+        if agentIndex == 0: #Pacman
+            return self.max_pacman(gameState, agentIndex, depth, alpha, beta)[1]
+        else:  #Ghosts
+            return self.min_ghost(gameState, agentIndex, depth, alpha, beta)[1]
+
+    def max_pacman(self, gameState, agentIndex, depth, alpha, beta):
+        highestAction = ("",-float('inf'))
+        for action in gameState.getLegalActions(agentIndex):
+            succAction = (action,self.alphabeta(gameState.generateSuccessor(agentIndex,action), (depth + 1) % gameState.getNumAgents(), depth+1, alpha, beta))
+            highestAction = max(highestAction,succAction,key=lambda x:x[1])  #We want to compare action value for each of them and take the maximum one
+
+            #If prunning
+            if highestAction[1] > beta:
+                return highestAction
+            else:
+                alpha = max(alpha, highestAction[1]) #Alpha assignment
+
+        return highestAction
+
+    def min_ghost(self, gameState, agentIndex, depth, alpha, beta):
+        lowestAction = ("",float('inf'))
+        for action in gameState.getLegalActions(agentIndex):
+            succAction = (action,self.alphabeta(gameState.generateSuccessor(agentIndex,action), (depth + 1) % gameState.getNumAgents(), depth+1, alpha, beta))
+            lowestAction = min(lowestAction,succAction,key=lambda x:x[1])  #We want to compare action value for each of them and take the minimum one
+
+            # Prunning
+            if lowestAction[1] < alpha:
+                return lowestAction
+            else:
+                beta = min(beta, lowestAction[1]) #Beta assignment
+
+        return lowestAction
+
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
