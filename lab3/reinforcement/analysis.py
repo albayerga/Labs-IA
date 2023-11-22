c34573f8f748429cff14d239b15d581fc1942364
albayerga <alba.yerga01@estudiant.upf.edu>
2023-11-22 12:11:41 +0100

    ej 2 y 3

--- lab3/reinforcement/analysis.py
+++ lab3/reinforcement/analysis.py
@@ -21,41 +21,50 @@
 
 def question2():
     answerDiscount = 0.9
-    answerNoise = 0.2
+    answerNoise = 0 #changed value -> ignore cliff
     return answerDiscount, answerNoise
+"""
+the grid only has one exit, so the discount factor should be high to prioritize long term rewards
+and the noise should be 0 to ignore the cliff and have some chance of reaching the exit
+"""
 
-def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+#higher discount factor -> prioritize long term rewards
+#lower discount factor -> prioritize short term rewards
+#higher noise -> avoids risks
+#lower noise -> takes risks (0 -> ignores risks)
+
+def question3a(): #prefer close exit (+1), risking cliff (-10)
+    answerDiscount = 0.2 #focus on short term rewards
+    answerNoise = 0 #ignore cliff
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3b(): #prefer close exit (+1), but avoiding cliff (-10)
+    answerDiscount = 0.4 #focus on short term rewards
+    answerNoise = 0.3 #avoid cliff
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3c(): #prefer distant exit (+10), risking cliff (-10)
+    answerDiscount = 1 #focus on long term rewards
+    answerNoise = 0 #ignore cliff
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3d(): #prefer distant exit (+10), avoiding cliff (-10)
+    answerDiscount = 0.9 #focus on long term rewards
+    answerNoise = 0.4 #avoid cliff
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3e(): #avoid both exits and cliff
+    answerDiscount = 0 #ignore rewards
+    answerNoise = 0 #ignore cliff
+    answerLivingReward = 0 
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
